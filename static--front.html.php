f93dbfe50464faefe0f436297a3e1bbed7e13f9d
danpros <dan@danpros.com>
2024-02-16 14:16:00 +0700

    Update static--front.html.php

--- static--front.html.php
+++ static--front.html.php
@@ -1,7 +1,7 @@
 <?php if (!defined('HTMLY')) die('HTMLy'); ?>
 <div class="profile">
 	<div class="profile_inner">
-		<img src="<?php echo theme_path();?>img/avatar.png" alt="<?php echo $static->title;?>">
+		<img src="<?php echo theme_path();?>img/avatar.png" height="150" width="150" alt="<?php echo $static->title;?>">
 		<h1><?php echo $static->title;?></h1>
 		<?php if (login()):?><a href="<?php echo site_url();?>front/edit?destination=front"><?php echo i18n('edit');?></a><?php endif;?>
 		<div class="profile_content"><?php echo $static->body;?></div>
